99e7b040e7671d77660cf3002b0d53b1eb5a63e0
chen <602042954@qq.com>
2024-03-23 14:15:59 +0800

    master

---
+++ study_2024_3_23/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    // double a = 5 % 2;//商2余1
+    // printf("a = %lf\n", a);
+    // return 0;
+
+    int a = 16;
+    //>> -- 右移操作符
+    //移动的是二进制位
+    //10000
+    int b = a >> 1;
+    printf("b = %d\n", b);
+
+
+
+
+    
+    return 0;
+}

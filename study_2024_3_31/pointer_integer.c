6aab716e995a810fb21808d1a0635334305f9f5b
chen <602042954@qq.com>
2024-03-31 22:05:09 +0800

    main

---
+++ study_2024_3_31/pointer_integer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define N_VALUES 5
+
+// int main()
+// {
+//     char ch[5] = {0};
+//     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     // int i = 0;
+//     // int sz = sizeof(arr) / sizeof(arr[0]);
+//     // int *p = arr;
+//     // for (i = 0; i < sz; i++)
+//     // {
+//     //     printf("%d ", *p);
+//     //     p = p + 1;
+//     // }
+
+//     //指针-指针 得到中间的元素个数
+//     printf("%d\n",&arr[9] - &arr[0]);
+//     // printf("%d\n",&arr[9] - &ch[0]);//错的
+//     return 0;
+// }

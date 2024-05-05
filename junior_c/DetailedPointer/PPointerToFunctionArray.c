ebb7c0a276fa539aacaa66141288dbd33367de65
chen <602042954@qq.com>
2024-05-05 15:32:47 +0800

    main

--- junior_c/DetailedPointer/PPointerToFunctionArray.c
+++ junior_c/DetailedPointer/PPointerToFunctionArray.c
@@ -13,6 +13,7 @@ int main()
 
     int (*pfArr[4])(int, int); // pfArr是一个数组-函数指针的数组
     int (*(*ppfArr)[4])(int, int) = &pfArr;
-
+    //ppfArr 是一个数组指针 指针指向的数组有4个元素
+    //指向的数组的每个元素的类型是一个函数指针
     return 0;
 }
